dc4f17b6a6e498ce710417b660a267f55d591d94
K-O-G <KaterinaO.Gordiyenko@livenau.net>
2017-02-05 14:40:10 +0200

    HashTable3

---
+++ src/HashTable/HashTable3.java
@@ -0,0 +1,62 @@
+package HashTable;
+
+import HashElement.Section;
+
+/**
+ * Created by Home on 04.02.2017.
+ */
+public class HashTable3 {
+    private Section HashTableArray[];
+    private int size;
+
+    public HashTable3(int size){
+        HashTableArray =new Section[size];
+        this.size=size;
+    }
+
+    public boolean insert(Section HashObject){
+        int index=(HashObject.angleOX()% HashTableArray.length);
+        if (HashTableArray[index]!=null){
+            boolean flag = false;
+            for (int i = index; i < HashTableArray.length; i++) {
+                int tempIndex = (index+i)%HashTableArray.length;
+                if (HashTableArray[tempIndex]== null){
+                    HashTableArray[tempIndex]= HashObject;
+                    return true;
+                }
+                if (i==HashTableArray.length-1){
+                    i=0;
+                    flag=true;
+                }
+                if (i == index && flag == true) {
+                    return false;
+                }
+            }
+        }
+        HashTableArray[index]=HashObject;
+        return true;
+    }
+
+    public void printHashTable(){
+        for (int i = 0; i < HashTableArray.length; i++) {
+            if (HashTableArray[i]==null) {
+                System.out.printf("%3d %s\n", i, "Position is null");
+            }
+            else{
+                System.out.printf("%3d %5d %s\n", i, HashTableArray[i].angleOX(), HashTableArray[i].toString());
+            }
+        }
+    }
+
+    public void delete(int length){
+        for (int i = 0; i < HashTableArray.length; i++) {
+            if (HashTableArray[i].lengthSection()>length){
+                HashTableArray[i]=null;
+            }
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
