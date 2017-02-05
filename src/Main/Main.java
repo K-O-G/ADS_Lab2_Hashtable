b5e624cbc86be98d2109fd760ac9330c17432162
K-O-G <KaterinaO.Gordiyenko@livenau.net>
2017-02-05 14:40:44 +0200

    Main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,12 +2,13 @@ package Main;
 
 
 import HashElement.Section;
-import HashTable.HashTable;
+import HashTable.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        level3();
     }
 
     public static void level1()
@@ -22,11 +23,24 @@ public class Main {
 
     public static void level2()
     {
-        HashTable hashTable = new HashTable(10);
+        HashTable2 hashTable = new HashTable2(10);
         for (int i = 0; i < hashTable.getSize(); i++) {
             Section section = new Section();
             hashTable.insert(section);
         }
+        hashTable.printHashTable();
+    }
+
+    public static void level3()
+    {
+        HashTable3 hashTable = new HashTable3(10);
+        for (int i = 0; i < hashTable.getSize(); i++) {
+            Section section = new Section();
+            hashTable.insert(section);
+        }
+        hashTable.printHashTable();
+        System.out.println("Delete");
+        hashTable.delete(50);
         hashTable.printHashTable();
     }
 }
