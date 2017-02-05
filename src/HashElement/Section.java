7636129059a934fb8a79d6f02ae6cdce88744c55
K-O-G <KaterinaO.Gordiyenko@livenau.net>
2017-02-05 15:50:12 +0200

    return degree in angleOX

--- src/HashElement/Section.java
+++ src/HashElement/Section.java
@@ -7,28 +7,40 @@ public class Section {
     Dot A;
     Dot B;
 
-    public Section(){
-        A= new Dot();
-        B= new Dot();
-        if(A.getX()==B.getX()&&A.getY()==B.getY()){
-            A= new Dot();
-            B= new Dot();
+    public Section() {
+        A = new Dot();
+        B = new Dot();
+        while (A.getX() == B.getX() && A.getY() == B.getY()) {
+            A = new Dot();
+            B = new Dot();
         }
     }
 
     @Override
     public String toString() {
         //return "A: ("+A.getX()+","+A.getY()+"); B: "+B.getX()+","+B.getY();
-        return String.format("A (%d, %d); B (%d, %d)",A.getX(),A.getY(),B.getX(),B.getY());
+        return String.format("A(%3d,%3d); B(%3d,%3d)", A.getX(), A.getY(), B.getX(), B.getY());
     }
 
-    public double lengthSection(){
+    public double lengthSection() {
         //A = √ ((X2-X1)²+(Y2-Y1)²).
-        return Math.pow((B.getX()-A.getX()),2)+ Math.pow((B.getY()-A.getY()),2);
+        return Math.sqrt(Math.pow((B.getX() - A.getX()), 2) + Math.pow((B.getY() - A.getY()), 2));
     }
 
-    public double angleOX(){
+    public int angleOX() {
         //tg f= ((y2-y1)/(x2-x1))
-        return Math.tan((B.getY()-A.getY())/(B.getX()-A.getX()));
+        //return (int)(Math.tan((B.getY()-A.getY())/(B.getX()-A.getX())));
+        if (B.getY() == A.getY()) {
+            return 0;
+        } else if (A.getX() == B.getX()) {
+            return 90;
+        }
+        else {
+            float fi = ((float) (B.getY() - A.getY()) / (B.getX() - A.getX()));
+            if (fi<0){
+                return (int)(fi*(-1));
+            }
+            else return (int)(fi);
+        }
     }
 }
